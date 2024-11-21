23e3ce0c7815516b69b6b38c83788de3d7bb7515
jingtao8a <1921050615@qq.com>
2024-11-21 20:40:11 +0800

    add competitor(lipp and ALEX)

--- src/benchmark/benchmark.h
+++ src/benchmark/benchmark.h
@@ -6,6 +6,8 @@
 
 #include "afli/afli.h"
 #include "nfl/nfl.h"
+#include "lipp/src/core/lipp.h"
+#include "ALEX/src/core/alex.h"
 
 namespace nfl {
 //configs 目录下的文件中可以添加 bucket_size aggregate_size (key)
@@ -106,12 +108,16 @@ public:
     bool show_stat = true;
     ExperimentalResults exp_res(batch_size);
     if (start_with(index_name, "afli")) {
-      run_afli(batch_size, exp_res, config_path, show_stat);
+        run_afli(batch_size, exp_res, config_path, show_stat);
     } else if (start_with(index_name, "nfl")) {
-      run_nfl(batch_size, exp_res, config_path, show_stat);
+        run_nfl(batch_size, exp_res, config_path, show_stat);
+    } else if (start_with(index_name, "lipp")) {
+        run_lipp(batch_size, exp_res, config_path, show_stat);
+    } else if (start_with(index_name, "alex")) {
+        run_alex(batch_size, exp_res, config_path, show_stat);
     } else {
-      std::cout << "Unsupported model name [" << index_name << "]" << std::endl;
-      exit(-1);
+        std::cout << "Unsupported model name [" << index_name << "]" << std::endl;
+        exit(-1);
     }
     // Print results.
     std::cout << workload_name << "\t" << index_name << "\t" << batch_size 
@@ -258,6 +264,120 @@ public:
       nfl.print_stats();
     }
   }
+
+  void run_lipp(int batch_size, ExperimentalResults& exp_res,
+                 std::string config_path, bool show_stat=false) {
+    // Start to bulk load
+      auto bulk_load_start = std::chrono::high_resolution_clock::now();
+      LIPP<KT, VT> lipp;
+      lipp.bulk_load(init_data.data(), init_data.size());
+      auto bulk_load_end = std::chrono::high_resolution_clock::now();
+      exp_res.bulk_load_index_time =
+              std::chrono::duration_cast<std::chrono::nanoseconds>(bulk_load_end
+                                                                   - bulk_load_start).count();
+      std::vector<KVT> batch_data;
+      batch_data.reserve(batch_size);
+      // Perform requests in batch
+      int num_batches = std::ceil(requests.size() * 1. / batch_size);
+      exp_res.latencies.reserve(num_batches * 3);
+      exp_res.need_compute.reserve(num_batches * 3);
+
+      for (int batch_idx = 0; batch_idx < num_batches; ++ batch_idx) {
+          batch_data.clear();
+          int l = batch_idx * batch_size;
+          int r = std::min((batch_idx + 1) * batch_size,
+                           static_cast<int>(requests.size()));
+          for (int i = l; i < r; ++ i) {
+              batch_data.push_back(requests[i].kv);
+          }
+
+          VT val_sum = 0;
+          // Perform requests
+          auto start = std::chrono::high_resolution_clock::now();
+          for (int i = l; i < r; ++ i) {
+              int data_idx = i - l;
+              if (requests[i].op == kQuery) {
+                  auto it = lipp.at(batch_data[data_idx].first);
+                  val_sum += it;
+              } else if (requests[i].op == kInsert) {
+                  lipp.insert(batch_data[data_idx]);
+              }
+          }
+          auto end = std::chrono::high_resolution_clock::now();
+          double time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+          exp_res.sum_indexing_time += time;
+          exp_res.num_requests += batch_data.size();
+          exp_res.latencies.push_back({0, time});
+          exp_res.step();
+      }
+      exp_res.model_size = lipp.model_size();
+      exp_res.index_size = lipp.index_size();
+      if (show_stat) {
+          lipp.print_depth();
+      }
+  }
+
+    void run_alex(int batch_size, ExperimentalResults& exp_res,
+                  std::string config_path, bool show_stat=false) {
+        // Start to bulk load
+        auto bulk_load_start = std::chrono::high_resolution_clock::now();
+        alex::Alex<KT, VT> alex;
+        alex.bulk_load(init_data.data(), init_data.size());
+        auto bulk_load_end = std::chrono::high_resolution_clock::now();
+        exp_res.bulk_load_index_time =
+                std::chrono::duration_cast<std::chrono::nanoseconds>(bulk_load_end
+                                                                     - bulk_load_start).count();
+
+        std::vector<KVT> batch_data;
+        batch_data.reserve(batch_size);
+        // Perform requests in batch
+        int num_batches = std::ceil(requests.size() * 1. / batch_size);
+        exp_res.latencies.reserve(num_batches * 3);
+        exp_res.need_compute.reserve(num_batches * 3);
+        for (int batch_idx = 0; batch_idx < num_batches; ++ batch_idx) {
+            batch_data.clear();
+            int l = batch_idx * batch_size;
+            int r = std::min((batch_idx + 1) * batch_size,
+                             static_cast<int>(requests.size()));
+            for (int i = l; i < r; ++ i) {
+                batch_data.push_back(requests[i].kv);
+            }
+
+            VT val_sum = 0;
+            // Perform requests
+            auto start = std::chrono::high_resolution_clock::now();
+            for (int i = l; i < r; ++ i) {
+                int data_idx = i - l;
+                if (requests[i].op == kQuery) {
+                    auto res = alex.find(batch_data[data_idx].first);
+                    if (res != alex.end()) {
+                        val_sum += res.payload();
+                    }
+                } else if (requests[i].op == kUpdate) {
+                    auto res = alex.find(batch_data[data_idx].first);
+                    if (res != alex.end()) {
+                        res.payload() = batch_data[data_idx].second;
+                    }
+                } else if (requests[i].op == kInsert) {
+                    alex.insert(batch_data[data_idx].first, batch_data[data_idx].second);
+                } else if (requests[i].op == kDelete) {
+                    int res = alex.erase(batch_data[data_idx].first);
+                }
+            }
+            auto end = std::chrono::high_resolution_clock::now();
+            double time = std::chrono::duration_cast<std::chrono::nanoseconds>(end
+                                                                               - start).count();
+            exp_res.sum_indexing_time += time;
+            exp_res.num_requests += batch_data.size();
+            exp_res.latencies.push_back({0, time});
+            exp_res.step();
+        }
+        exp_res.model_size = alex.model_size();
+        exp_res.index_size = alex.model_size() + alex.data_size();
+        if (show_stat) {
+            alex.print_stats();
+        }
+    }
 };
 
 }
