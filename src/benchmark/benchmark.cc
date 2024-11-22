e401beb23707d03f6823e8ef31b5ddbcbb3d6c9b
jingtao8a <1921050615@qq.com>
2024-11-22 08:55:33 +0800

    update benchmark

--- src/benchmark/benchmark.cc
+++ src/benchmark/benchmark.cc
@@ -3,18 +3,20 @@
 using namespace nfl;
 
 int main(int argc, char* argv[]) {
-  if (argc < 4) {
+  if (argc < 3) {
     std::cout << "No enough parameters" << std::endl;
-    std::cout << "Please input: benchmark (index name) (batch size) "
+    std::cout << "Please input: benchmark (index name) "
               << "(workload path) [config path] "
               << "[show incremental updates]" << std::endl;
     exit(-1);
   }
   std::string index_name = std::string(argv[1]);
-  int batch_size = std::stoi(argv[2]);
-  std::string workload_path = std::string(argv[3]);
-  std::string config_path = argc > 4 ? std::string(argv[4]) : "";
-  std::string show_inc_thro = argc > 5 ? std::string(argv[5]) : "";
+  std::string workload_path = std::string(argv[2]);
+  std::string config_path = argc > 3 ? std::string(argv[3]) : "";
+  std::string show_inc_thro = argc > 4 ? std::string(argv[4]) : "";
+  std::string workload_name = get_workload_name(workload_path);
+  auto index = workload_name.rfind("_");
+  int batch_size = std::stoi(workload_name.substr(index + 1, workload_name.length() - index - 1));
   srand(kSEED);
   Benchmark<double, long long> benchmark;
   benchmark.run_workload(index_name, batch_size, workload_path, config_path, show_inc_thro != "");
