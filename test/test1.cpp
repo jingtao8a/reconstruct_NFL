2632121d957c690d5fb5cacc2bc2eacc566369e9
jingtao8a <1921050615@qq.com>
2024-11-22 08:55:53 +0800

    update test1.cpp

--- test/test1.cpp
+++ test/test1.cpp
@@ -2,9 +2,9 @@
 // Created by chengang on 24-11-21.
 //
 
-#include "test1.h"
 #include <string>
 #include <iostream>
+#include "util/common.h"
 
 int get_num_keys(std::string name) {
     int tot_num = 0;
@@ -22,17 +22,22 @@ int get_num_keys(std::string name) {
 }
 
 int main() {
-    std::string path("/data/ycsb-200M.bin.data");
-    int start;
-    if (path.rfind('/') == std::string::npos) {
-        start = 0;
-    } else {
-        start = path.rfind('/') + 1;
-    }
-    int end = path.rfind(".bin.data");
-    int length = end - start;
-    auto data_name = path.substr(start, length);
-    std::cout << data_name << std::endl;
-    std::cout << get_num_keys(path) << std::endl;
+//    std::string path("/data/ycsb-200M.bin.data");
+//    int start;
+//    if (path.rfind('/') == std::string::npos) {
+//        start = 0;
+//    } else {
+//        start = path.rfind('/') + 1;
+//    }
+//    int end = path.rfind(".bin.data");
+//    int length = end - start;
+//    auto data_name = path.substr(start, length);
+//    std::cout << data_name << std::endl;
+//    std::cout << get_num_keys(path) << std::endl;
+    std::string workload_path("/home/chengang/chengang/jingtao8a/reconstruct_NFL/workload/fb_200M_double_80R_zipf_10000.bin");
+    std::string workload_name = nfl::get_workload_name(workload_path);
+    auto index = workload_name.rfind("_");
+    int batch_size = std::stoi(workload_name.substr(index + 1, workload_name.length() - index - 1));
+    std::cout << batch_size << std::endl;
     return 0;
 }
