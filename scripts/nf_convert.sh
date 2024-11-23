8de704f4611c1d7c525f57478f07dba9eacc1d8c
jingtao8a <1921050615@qq.com>
2024-11-23 10:13:47 +0800

    add nf_convert.sh

---
+++ scripts/nf_convert.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e # fail and exit on any command erroring
+
+root_dir=$(cd `dirname $0`/..; pwd)
+build_dir=${root_dir}/cmake-build-debug
+exec=${build_dir}/nf_convert
+
+workload_path=/home/chengang/chengang/jingtao8a/reconstruct_NFL/workload/longlat_200M_double_80R_zipf_10000.bin
+proportion_of_keys=100
+flow_input_director=/home/chengang/chengang/jingtao8a/reconstruct_NFL/train_data
+
+echo "${exec} ${workload_path} ${proportion_of_keys} ${flow_input_director}"
+${exec} ${workload_path} ${proportion_of_keys} ${flow_input_director}
