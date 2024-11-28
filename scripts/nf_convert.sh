f5dc27caab9a6435491ae699c6200c42f140bd9c
jingtao8a <1921050615@qq.com>
2024-11-28 11:53:14 +0800

    update nf_convert.sh

--- scripts/nf_convert.sh
+++ scripts/nf_convert.sh
@@ -6,7 +6,7 @@ build_dir=${root_dir}/cmake-build-debug
 exec=${build_dir}/nf_convert
 
 workload_path=/home/chengang/chengang/jingtao8a/reconstruct_NFL/workload/longlat_200M_double_80R_zipf_10000.bin
-proportion_of_keys=1
+proportion_of_keys=100
 flow_input_director=/home/chengang/chengang/jingtao8a/reconstruct_NFL/train/data
 
 echo "${exec} ${workload_path} ${proportion_of_keys} ${flow_input_director}"
